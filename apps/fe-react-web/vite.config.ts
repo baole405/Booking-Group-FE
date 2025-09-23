2fc8454e69acb43657340f68bf620f288c01bb77
baole405 <baolttse184569@fpt.edu.vn>
2025-09-23 22:46:39 +0700

    feat: :arrow_up: thành công implement mcp shadcn

--- apps/fe-react-web/vite.config.ts
+++ apps/fe-react-web/vite.config.ts
@@ -1,12 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   root: __dirname,
   build: {
-    outDir: "../../dist/apps/fe-react-web",
+    outDir: "./dist",
     emptyOutDir: true,
+    reportCompressedSize: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
