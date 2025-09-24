600528a125818a266bdeb27430fe6432ee87a3f1
baole405 <baolttse184569@fpt.edu.vn>
2025-09-24 15:55:05 +0700

    chore: :zap: setup routing first with Homepage

---
+++ apps/fe-react-web/src/constants/route.constant.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  // Root paths
+  HOME: "/",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  DASHBOARD: "/dashboard",
+  PROFILE: "/profile",
+
+  //tự thêm
+};
--- apps/fe-react-web/src/main.tsx
+++ apps/fe-react-web/src/main.tsx
@@ -1,13 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-}
+// Tạo một instance của QueryClient
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
