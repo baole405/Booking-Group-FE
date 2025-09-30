c93ad1da99b12dc751d0d0d342f44307dea6e6b5
baole <tommydao2000@gmail.com>
2025-09-30 15:56:31 +0700

    feat: configure vitest support for fe-react-web

--- apps/fe-react-web/vite.config.ts
+++ apps/fe-react-web/vite.config.ts
@@ -1,11 +1,36 @@
+import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
+// These options were migrated by @nx/vite:convert-to-inferred from the project.json file.
+const configValues = { default: {}, development: {}, production: {} };
+
+// Determine the correct configValue to use based on the configuration
+const nxConfiguration = process.env.NX_TASK_TARGET_CONFIGURATION ?? "default";
+
+const options = {
+  ...configValues.default,
+  ...(configValues[nxConfiguration] ?? {}),
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  cacheDir: "../../node_modules/.vite/apps/fe-react-web",
+  test: {
+    watch: false,
+    globals: true,
+    environment: "jsdom",
+    include: ["{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    reporters: ["default"],
+    coverage: {
+      reportsDirectory: "../../coverage/apps/fe-react-web",
+      provider: "v8" as const,
+    },
+  },
+  plugins: [react(), tailwindcss(), nxViteTsPaths(), nxCopyAssetsPlugin(["*.md"])],
   root: __dirname,
   build: {
     outDir: "./dist",
