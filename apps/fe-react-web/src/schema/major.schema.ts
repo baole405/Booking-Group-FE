1c6cbfe90d0312206d98d1c111efaf3b1feb19f6
minhphd1203 <minhphdse184774@fpt.edu.vn>
2025-10-27 22:09:12 +0700

    feat: major

--- apps/fe-react-web/src/schema/major.schema.ts
+++ apps/fe-react-web/src/schema/major.schema.ts
@@ -3,6 +3,7 @@ import z from "zod";
 export const MajorSchema = z.object({
   id: z.number(),
   name: z.string(),
+  active: z.boolean(),
 });
 export const CreateMajorSchema = z.object({
   name: z.string().min(1, "Name is required"),
